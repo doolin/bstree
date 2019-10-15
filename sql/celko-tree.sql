f9736fd6b39e7b1ae7424adc52ffbda4d65c74fa
dave doolin <david.doolin@gmail.com>
2019-10-15 06:48:44 -0700

    more of the celko example

--- sql/celko-tree.sql
+++ sql/celko-tree.sql
@@ -1,14 +1,26 @@
 -- http://www.ibase.ru/files/articles/programming/dbmstrees/sqltrees.html
 
-drop table if exists Personnel1;
-CREATE TABLE Personnel1
-(emp CHAR(10) NOT NULL PRIMARY KEY,
-boss CHAR(10) DEFAULT NULL REFERENCES Personnel1(emp),
-salary DECIMAL(6,2) NOT NULL DEFAULT 100.00);
+-- sqlite3 celko.db < celko-tree.sql
+
+drop table if exists personnel1;
+CREATE TABLE personnel1 (
+  emp CHAR(10) NOT NULL PRIMARY KEY,
+  boss CHAR(10) DEFAULT NULL REFERENCES personnel1(emp),
+  salary DECIMAL(6,2) NOT NULL DEFAULT 100.00
+);
+
+insert into personnel1 (emp, boss, salary) values
+  ("albert",null,1000.00),
+  ("chuck", "albert", 900.00),
+  ("bert", "albert", 900.00),
+  ("donna", "chuck", 800.00),
+  ("eddie", "chuck", 700.00),
+  ("fred", "chuck", 600.00);
 
 drop table if exists Personnel2;
-CREATE TABLE Personnel2
-(emp CHAR(10) NOT NULL PRIMARY KEY,
+CREATE TABLE Personnel2 (
+  emp CHAR(10) NOT NULL PRIMARY KEY,
   lft INTEGER NOT NULL UNIQUE CHECK (lft > 0),
   rgt INTEGER NOT NULL UNIQUE CHECK (rgt > 1),
-  CONSTRAINT order_okay CHECK (lft < rgt) );
+  CONSTRAINT order_okay CHECK (lft < rgt)
+);
