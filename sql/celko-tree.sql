a4a0ccf892db9b006a01468e7d7395f0050c8523
dave doolin <david.doolin@gmail.com>
2019-10-19 05:12:52 -0700

    more celko

--- sql/celko-tree.sql
+++ sql/celko-tree.sql
@@ -17,6 +17,22 @@ insert into personnel1 (emp, boss, salary) values
   ("eddie", "chuck", 700.00),
   ("fred", "chuck", 600.00);
 
+drop table if exists salaries;
+CREATE TABLE salaries (
+  emp CHAR(10) NOT NULL PRIMARY KEY,
+  boss CHAR(10) DEFAULT NULL REFERENCES salaries(emp),
+  salary DECIMAL(6,2) NOT NULL DEFAULT 100.00
+);
+
+insert into salaries (emp, boss, salary) values
+  ("albert",null,1000.00),
+  ("chuck", "albert", 900.00),
+  ("bert", "albert", 900.00),
+  ("donna", "chuck", 800.00),
+  ("eddie", "chuck", 700.00),
+  ("fred", "chuck", 600.00);
+
+
 drop table if exists personnel2;
 CREATE TABLE personnel2 (
   emp CHAR(10) NOT NULL PRIMARY KEY,
@@ -43,8 +59,9 @@ SELECT P1.* FROM personnel2 AS P1, personnel2 AS P2
     WHERE P1.lft BETWEEN P2.lft AND P2.rgt
     AND P2.emp = :myemployee;
 
--- SELECT P2.emp, SUM(S1.salary) FROM personnel2 AS P1, personnel2 AS P2, Salaries AS S1
---    WHERE P1.lft BETWEEN P2.lft AND P2.rgt AND P1.emp = S1.emp GROUP BY P2.emp;
+-- Salaries by control
+SELECT P2.emp, SUM(S1.salary) FROM personnel2 AS P1, personnel2 AS P2, Salaries AS S1
+   WHERE P1.lft BETWEEN P2.lft AND P2.rgt AND P1.emp = S1.emp GROUP BY P2.emp;
 
 -- This one works
 SELECT COUNT(P2.emp) AS indentation, P1.emp FROM personnel2 AS P1, personnel2 AS P2
