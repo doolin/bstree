d3ea49db3e6c20b95951b758bedac6c4246723a3
Dave Doolin <david.doolin@gmail.com>
2020-11-13 06:32:13 -0800

    BST-29 Fix ruby generator to be correct for all tree sizes given (#32)

--- ruby/spec/generator_spec.rb
+++ ruby/spec/generator_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-# TODO: these are documented somewhere, figure out where
-# the docs are located.
+# Documented in examples.tex
 RSpec.describe Generator do
   it 'builds tree1' do
     tree1 = Generator.tree1
@@ -15,7 +14,7 @@ RSpec.describe Generator do
     tree2 = Generator.tree2
     expect(tree2.size).to eq 2
     expect(tree2.bst?).to be true
-    expect(tree2.full?).to be false # nil
+    expect(tree2.full?).to be false
     # expect(tree.pathological??).to be false
   end
 
@@ -29,49 +28,49 @@ RSpec.describe Generator do
 
   it 'builds tree4' do
     tree = Generator.tree4
-    expect(tree.size).to eq 3
+    expect(tree.size).to eq 4
     expect(tree.bst?).to be true
-    expect(tree.full?).to be false # nil
+    expect(tree.full?).to be false
     # expect(tree.pathological??).to be true
   end
 
   it 'builds tree5' do
     tree = Generator.tree5
-    expect(tree.size).to eq 3
+    expect(tree.size).to eq 5
     expect(tree.bst?).to be true
-    expect(tree.full?).to be false # nil
+    expect(tree.full?).to be false
     # expect(tree.pathological??).to be true
   end
 
   it 'builds tree6' do
     tree = Generator.tree6
-    expect(tree.size).to eq 5
+    expect(tree.size).to eq 6
     expect(tree.bst?).to be true
-    expect(tree.full?).to be true
+    expect(tree.full?).to be false
     # expect(tree.pathological??).to be true
   end
 
   it 'builds tree7' do
     tree = Generator.tree7
-    expect(tree.size).to eq 5
+    expect(tree.size).to eq 7
     expect(tree.bst?).to be true
-    expect(tree.full?).to be true
+    expect(tree.full?).to be false
     # expect(tree.pathological??).to be true
   end
 
   it 'builds tree8' do
     tree = Generator.tree8
-    expect(tree.size).to eq 7
+    expect(tree.size).to eq 8
     expect(tree.bst?).to be true
-    expect(tree.full?).to be true
+    expect(tree.full?).to be false
     # expect(tree.pathological??).to be true
   end
 
   it 'builds tree9' do
     tree = Generator.tree9
-    expect(tree.size).to eq 7
+    expect(tree.size).to eq 9
     expect(tree.bst?).to be true
-    expect(tree.full?).to be false # nil
+    expect(tree.full?).to be false
     # expect(tree.pathological??).to be true
   end
 
