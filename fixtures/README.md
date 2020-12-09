05704651c8951cacd42a44bbd40c2b99f4dd70db
Dave Doolin <david.doolin@gmail.com>
2020-12-09 04:35:24 -0800

    BST-44 create a yaml fixture directory at repo root (#38)
    From the README:
    
    Normally, test fixtures would go into a directory
    structure local to a specific implementation. In this
    case, a set of fixtures common to all implementations
    is useful.
    
    This commit uses the Ruby implementation as an initial
    proof of concept. The Ruby tree Generator class was used
    to create the initial yaml fixtures, and all of the example
    trees defined in the Generator class are written to yaml.
    Some modifications had to be made to allow a pre-specified
    uuid to be defined for the nodes, otherwise it's really
    hard to test nodes by comparison when the uuid is truly
    random.
    
    Note that Ruby has a built-in `to_yaml` method for objects.
    The decision here is to not override that method, but
    instead implement `to_yml` to store just the data for each
    tree, without any of the Ruby metadata associated with
    the Ruby built-in `to_yaml`.

---
+++ fixtures/README.md
@@ -0,0 +1,6 @@
+# Binary Search Tree fixtures
+
+Normally, test fixtures would go into a directory
+structure local to a specific implementation. In this
+case, a set of fixtures common to all implementations
+is useful.
--- ruby/lib/generator.rb
+++ ruby/lib/generator.rb
@@ -5,72 +5,72 @@ require_relative './node'
 
 class Generator
   class << self
-    def build(nodes)
-      tree = Tree.new(Node.new(nodes.shift))
-      nodes.each { |n| tree.insert(Node.new(n)) }
+    def build(nodes, uuid)
+      tree = Tree.new(Node.new(nodes.shift, uuid))
+      nodes.each { |n| tree.insert(Node.new(n, uuid)) }
       tree
     end
 
-    def tree1
-      Generator.build [11]
+    def tree1(uuid = nil)
+      Generator.build [11], uuid
     end
 
-    def tree2
-      Generator.build [11, 7]
+    def tree2(uuid = nil)
+      Generator.build [11, 7], uuid
     end
 
-    def tree3
-      Generator.build [11, 7, 13]
+    def tree3(uuid = nil)
+      Generator.build [11, 7, 13], uuid
     end
 
-    def tree4
-      Generator.build [11, 7, 13, 3]
+    def tree4(uuid = nil)
+      Generator.build [11, 7, 13, 3], uuid
     end
 
-    def tree5
-      Generator.build [11, 7, 13, 3, 19]
+    def tree5(uuid = nil)
+      Generator.build [11, 7, 13, 3, 19], uuid
     end
 
-    def tree6
-      Generator.build [11, 7, 13, 3, 19, 29]
+    def tree6(uuid = nil)
+      Generator.build [11, 7, 13, 3, 19, 29], uuid
     end
 
-    def tree7
-      Generator.build [11, 7, 13, 3, 19, 29, 5]
+    def tree7(uuid = nil)
+      Generator.build [11, 7, 13, 3, 19, 29, 5], uuid
     end
 
-    def tree8
-      Generator.build [11, 7, 13, 3, 19, 29, 5, 2]
+    def tree8(uuid = nil)
+      Generator.build [11, 7, 13, 3, 19, 29, 5, 2], uuid
     end
 
-    def tree9
-      Generator.build [11, 7, 13, 3, 19, 29, 5, 2, 17]
+    def tree9(uuid = nil)
+      Generator.build [11, 7, 13, 3, 19, 29, 5, 2, 17], uuid
     end
 
-    def tree10
-      Generator.build [11, 7, 13, 3, 19, 29, 5, 2, 17, 23]
+    def tree10(uuid = nil)
+      Generator.build [11, 7, 13, 3, 19, 29, 5, 2, 17, 23], uuid
     end
 
     # These could go into yaml files in spec/fixtures, or even in a
     # globally accessible file for use by all implementations.
-    def tree213
-      Generator.build [2, 1, 3]
+    def tree213(uuid = nil)
+      Generator.build [2, 1, 3], uuid
     end
 
-    def tree123
-      Generator.build [1, 2, 3]
+    def tree123(uuid = nil)
+      Generator.build [1, 2, 3], uuid
     end
 
-    def tree132
-      Generator.build [1, 3, 2]
+    def tree132(uuid = nil)
+      Generator.build [1, 3, 2], uuid
     end
 
-    def tree321
-      Generator.build [3, 2, 1]
+    def tree321(uuid = nil)
+      Generator.build [3, 2, 1], uuid
     end
 
-    def tree312
-      Generator.build [3, 1, 2]
+    def tree312(uuid = nil)
+      Generator.build [3, 1, 2], uuid
     end
   end
 end
