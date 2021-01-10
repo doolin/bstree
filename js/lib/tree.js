0dfb54b299b9733e9a6988f395b484dc9bfc1972
dave doolin <david.doolin@gmail.com>
2021-01-10 05:22:12 -0800

    BST-70 Install eslint for javascript

--- js/lib/tree.js
+++ js/lib/tree.js
@@ -1,33 +1,37 @@
-var Tree = function () {
+const Tree = function() {
   this.root = null;
 };
 
-Tree.prototype.log = function (msg) {
+Tree.prototype.log = function(msg) {
   console.log(msg);
 };
 
-Tree.prototype.insert = function (n) {
+Tree.prototype.insert = function(n) {
   this.root === null ? this.root = n : this.root.insert(n);
-}
+};
 
-Tree.prototype.is_empty = function () {
+Tree.prototype.is_empty = function() {
   return this.root === null;
 };
 
 Tree.prototype.collect = function(collector) {
-  if (this.root === null) { return; }
+  if (this.root === null) {
+    return;
+  }
   this.root.collect(collector);
 };
 
 Tree.prototype.list_keys = function() {
   if (this.root === null) return [];
   return this.root.list_keys();
-}
+};
 
 Tree.prototype.search = function(key) {
-  if (this.root === null) { return false; }
+  if (this.root === null) {
+    return false;
+  }
   return this.root.search(key);
-}
+};
 
 Tree.prototype.transplant = function(u, v) {
   if (u === this.root) {
@@ -41,7 +45,7 @@ Tree.prototype.transplant = function(u, v) {
   if (v !== null) {
     v.parent = u.parent;
   }
-}
+};
 
 Tree.prototype.delete_node = function(key) {
   z = this.search(key);
@@ -65,50 +69,66 @@ Tree.prototype.delete_node = function(key) {
 
   z.unlink();
   return z;
-}
+};
 
 Tree.prototype.is_present = function(key) {
-  if (this.root === null) { return false; }
+  if (this.root === null) {
+    return false;
+  }
   return this.root.is_present(key);
-}
+};
 
 Tree.prototype.maximum = function() {
-  if (this.root === null) { return null; }
+  if (this.root === null) {
+    return null;
+  }
   return this.root.maximum();
-}
+};
 
 Tree.prototype.minimum = function() {
-  if (this.root === null) { return null; }
+  if (this.root === null) {
+    return null;
+  }
   return this.root.minimum();
-}
+};
 
 Tree.prototype.successor = function(node) {
-  if (this.root === null) { return null; }
+  if (this.root === null) {
+    return null;
+  }
   return this.root.successor(node);
-}
+};
 
 Tree.prototype.predecessor = function(node) {
-  if (this.root === null) { return null; }
+  if (this.root === null) {
+    return null;
+  }
   return this.root.predecessor(node);
-}
+};
 
 Tree.prototype.is_bst = function() {
-  if (this.root === null) { return true; }
+  if (this.root === null) {
+    return true;
+  }
   return this.root.is_bst();
-}
+};
 
 Tree.prototype.is_empty = function() {
   return (this.root === null);
-}
+};
 
 Tree.prototype.height = function() {
-  if (this.root === null) { return 0; }
+  if (this.root === null) {
+    return 0;
+  }
   return this.root.height();
-}
+};
 
 Tree.prototype.size = function() {
-  if (this.root === null) { return 0; }
+  if (this.root === null) {
+    return 0;
+  }
   return this.root.size();
-}
+};
 
 exports.Tree = Tree;
