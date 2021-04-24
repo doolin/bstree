832fee1c5d821979b3f1b2abd77a3e18b6754d86
dave doolin <david.doolin@gmail.com>
2021-04-24 14:16:47 -0700

    BST-111 read from yaml in elixir test
    This is just a proof of concept to ensure the elixir
    code can read from existing fixtures.
    
    Some dependencies needed to be updated to be able to
    complie the tests, those updates are rolled into this
    change as well.

--- elixir/test/bst_test.exs
+++ elixir/test/bst_test.exs
@@ -2,11 +2,16 @@ defmodule BstTest do
   use ExUnit.Case
   doctest Bst
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
   test 'testem' do
     assert Bst.Node.testem == 'testem'
+  end
+
+  test 'read yaml fixture' do
+    path = Path.join(File.cwd!(), "../fixtures/tree1.yml")
+    expected = {:ok, %{"key" => 11, "left" => nil, "right" => nil, "uuid" => "uuid"}}
+    actual = YamlElixir.read_from_file(path)
+    IO.inspect(actual)
+
+    assert actual == expected
   end
 end
