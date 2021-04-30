6d621f20d3fdeb7fdc9dc22e58593948074df711
Dave Doolin <david.doolin@gmail.com>
2021-04-30 06:56:31 -0700

    BST-139 Spike on node insertion in elixir (#70)
    A partially successful spike but was unable to
    implement node insertion recursively.

--- elixir/lib/bst/node.ex
+++ elixir/lib/bst/node.ex
@@ -1,7 +1,25 @@
 defmodule Bst.Node do
   @moduledoc "My server code."
 
-  def testem do
-    'testem'
+  defstruct key: nil, value: nil, parent: nil, left: nil, right: nil
+
+  def insert(root, new) do
+    # IO.inspect(root)
+
+    if root.key < new.key do
+      if root.left == nil do
+        %Bst.Node{key: root.key, right: new}
+      else
+        # IO.inspect(root)
+        Bst.Node.insert(root.left, new)
+      end
+    else
+      if root.right == nil do
+        %Bst.Node{key: root.key, left: new}
+      else
+        # IO.inspect(root)
+        Bst.Node.insert(root.root, new)
+      end
+    end
   end
 end
--- elixir/test/bst_test.exs
+++ elixir/test/bst_test.exs
@@ -2,16 +2,17 @@ defmodule BstTest do
   use ExUnit.Case
   doctest Bst
 
-  test 'testem' do
-    assert Bst.Node.testem() == 'testem'
-  end
-
+  # TODO: convert the strings to atoms:
+  # https://stackoverflow.com/questions/41980358/convert-maps-to-struct
+  # then create a struct
+  #
+  # also, configure credo: https://hexdocs.pm/credo/config_file.html
   test 'read yaml fixture' do
     path = Path.join(File.cwd!(), "../fixtures/tree1.yml")
     expected = {:ok, %{"key" => 11, "left" => nil, "right" => nil, "uuid" => "uuid"}}
     actual = YamlElixir.read_from_file(path)
     # IO.inspect(actual, limit: :infinity)
-
+    # IO.inspect(actual)
     assert actual == expected
   end
 end
