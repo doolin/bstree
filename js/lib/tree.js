738f0d2b95259f0544c534ca878a84c97298248e
Dave Doolin <david.doolin@gmail.com>
2021-06-24 03:47:30 -0700

    BST-71 install and configure prettier for javascript (#119)

--- js/lib/tree.js
+++ js/lib/tree.js
@@ -1,39 +1,39 @@
-const Tree = function() {
+const Tree = function () {
   this.root = null;
 };
 
-Tree.prototype.log = function(msg) {
+Tree.prototype.log = function (msg) {
   console.log(msg);
 };
 
-Tree.prototype.insert = function(n) {
-  this.root === null ? this.root = n : this.root.insert(n);
+Tree.prototype.insert = function (n) {
+  this.root === null ? (this.root = n) : this.root.insert(n);
 };
 
-Tree.prototype.is_empty = function() {
+Tree.prototype.is_empty = function () {
   return this.root === null;
 };
 
-Tree.prototype.collect = function(collector) {
+Tree.prototype.collect = function (collector) {
   if (this.root === null) {
     return;
   }
   this.root.collect(collector);
 };
 
-Tree.prototype.list_keys = function() {
+Tree.prototype.list_keys = function () {
   if (this.root === null) return [];
   return this.root.list_keys();
 };
 
-Tree.prototype.search = function(key) {
+Tree.prototype.search = function (key) {
   if (this.root === null) {
     return false;
   }
   return this.root.search(key);
 };
 
-Tree.prototype.transplant = function(u, v) {
+Tree.prototype.transplant = function (u, v) {
   if (u === this.root) {
     this.root = v;
   } else if (u.parent.left === u) {
@@ -47,7 +47,7 @@ Tree.prototype.transplant = function(u, v) {
   }
 };
 
-Tree.prototype.delete_node = function(key) {
+Tree.prototype.delete_node = function (key) {
   z = this.search(key);
   // TODO: catch key not found
 
@@ -71,67 +71,67 @@ Tree.prototype.delete_node = function(key) {
   return z;
 };
 
-Tree.prototype.is_present = function(key) {
+Tree.prototype.is_present = function (key) {
   if (this.root === null) {
     return false;
   }
   return this.root.is_present(key);
 };
 
-Tree.prototype.maximum = function() {
+Tree.prototype.maximum = function () {
   if (this.root === null) {
     return null;
   }
   return this.root.maximum();
 };
 
-Tree.prototype.minimum = function() {
+Tree.prototype.minimum = function () {
   if (this.root === null) {
     return null;
   }
   return this.root.minimum();
 };
 
-Tree.prototype.successor = function(node) {
+Tree.prototype.successor = function (node) {
   if (this.root === null) {
     return null;
   }
   return this.root.successor(node);
 };
 
-Tree.prototype.predecessor = function(node) {
+Tree.prototype.predecessor = function (node) {
   if (this.root === null) {
     return null;
   }
   return this.root.predecessor(node);
 };
 
-Tree.prototype.isBst = function() {
+Tree.prototype.isBst = function () {
   if (this.root === null) {
     return true;
   }
   return this.root.isBst();
 };
 
-Tree.prototype.is_empty = function() {
-  return (this.root === null);
+Tree.prototype.is_empty = function () {
+  return this.root === null;
 };
 
-Tree.prototype.height = function() {
+Tree.prototype.height = function () {
   if (this.root === null) {
     return 0;
   }
   return this.root.height();
 };
 
-Tree.prototype.size = function() {
+Tree.prototype.size = function () {
   if (this.root === null) {
     return 0;
   }
   return this.root.size();
 };
 
-Tree.prototype.isBalanced = function() {
+Tree.prototype.isBalanced = function () {
   if (this.root === null) {
     return true;
   }
--- js/spec/from_yaml_spec.js
+++ js/spec/from_yaml_spec.js
@@ -1,64 +1,64 @@
-const assert = require('assert');
+const assert = require("assert");
 
-const fromYaml = require('../lib/from_yaml');
+const fromYaml = require("../lib/from_yaml");
 
-describe('load tree from yaml fixture', () => {
-  it('reads tree1 fixture and writes to console', () => {
-    const filenode = fromYaml('tree1.yml');
+describe("load tree from yaml fixture", () => {
+  it("reads tree1 fixture and writes to console", () => {
+    const filenode = fromYaml("tree1.yml");
 
     assert.strictEqual(filenode.size(), 1);
   });
 
-  it('reads tree2 fixture and writes to console', () => {
-    const filenode = fromYaml('tree2.yml');
+  it("reads tree2 fixture and writes to console", () => {
+    const filenode = fromYaml("tree2.yml");
 
     assert.strictEqual(filenode.size(), 2);
   });
 
-  it('reads tree3 fixture and writes to console', () => {
-    const filenode = fromYaml('tree3.yml');
+  it("reads tree3 fixture and writes to console", () => {
+    const filenode = fromYaml("tree3.yml");
 
     assert.strictEqual(filenode.size(), 3);
   });
 
-  it('reads tree4 fixture and writes to console', () => {
-    const filenode = fromYaml('tree4.yml');
+  it("reads tree4 fixture and writes to console", () => {
+    const filenode = fromYaml("tree4.yml");
 
     assert.strictEqual(filenode.size(), 4);
   });
 
-  it('reads tree5 fixture and writes to console', () => {
-    const filenode = fromYaml('tree5.yml');
+  it("reads tree5 fixture and writes to console", () => {
+    const filenode = fromYaml("tree5.yml");
 
     assert.strictEqual(filenode.size(), 5);
   });
 
-  it('reads tree6 fixture and writes to console', () => {
-    const filenode = fromYaml('tree6.yml');
+  it("reads tree6 fixture and writes to console", () => {
+    const filenode = fromYaml("tree6.yml");
 
     assert.strictEqual(filenode.size(), 6);
   });
 
-  it('reads tree7 fixture and writes to console', () => {
-    const filenode = fromYaml('tree7.yml');
+  it("reads tree7 fixture and writes to console", () => {
+    const filenode = fromYaml("tree7.yml");
 
     assert.strictEqual(filenode.size(), 7);
   });
 
-  it('reads tree8 fixture and writes to console', () => {
-    const filenode = fromYaml('tree8.yml');
+  it("reads tree8 fixture and writes to console", () => {
+    const filenode = fromYaml("tree8.yml");
 
     assert.strictEqual(filenode.size(), 8);
   });
 
-  it('reads tree9 fixture and writes to console', () => {
-    const filenode = fromYaml('tree9.yml');
+  it("reads tree9 fixture and writes to console", () => {
+    const filenode = fromYaml("tree9.yml");
 
     assert.strictEqual(filenode.size(), 9);
   });
 
-  it('reads tree10 fixture and writes to console', () => {
-    const filenode = fromYaml('tree10.yml');
+  it("reads tree10 fixture and writes to console", () => {
+    const filenode = fromYaml("tree10.yml");
 
     assert.strictEqual(filenode.size(), 10);
   });
