22a4fed8f178a2c48b2f967298513d972d12d732
dave doolin <david.doolin@gmail.com>
2021-06-27 05:24:02 -0700

    BST-194 Implement remaining property tests for yaml trees in javascript

--- js/lib/from_yaml.js
+++ js/lib/from_yaml.js
@@ -4,23 +4,27 @@ const yaml = require("js-yaml");
 // TODO: read up on shadowing for why this cannot be
 // name `node` in the context of using it below.
 const Node = require("./node.js").Node;
+const Tree = require("./tree.js").Tree;
 
 const fromYaml = (filename) => {
   const fileContents = fs.readFileSync(`../fixtures/${filename}`, "utf8");
   const data = yaml.load(fileContents);
 
-  const createTree = function (data) {
+  const traverseNodes = function (data) {
     if (data === null) {
       return null;
     }
 
     const node = new Node(data.key);
-    node.left = createTree(data.left);
-    node.right = createTree(data.right);
+    node.left = traverseNodes(data.left);
+    node.right = traverseNodes(data.right);
     return node;
   };
 
-  return createTree(data);
+  nodes = traverseNodes(data);
+  tree = new Tree();
+  tree.root = nodes;
+  return tree;
 };
 
 module.exports = fromYaml;
--- js/spec/from_yaml_spec.js
+++ js/spec/from_yaml_spec.js
@@ -7,59 +7,129 @@ describe("load tree from yaml fixture", () => {
     const filenode = fromYaml("tree1.yml");
 
     assert.strictEqual(filenode.size(), 1);
+    assert.strictEqual(filenode.height(), 0);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), true)
+    assert.strictEqual(filenode.isBalanced(), true);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree2 fixture and writes to console", () => {
     const filenode = fromYaml("tree2.yml");
 
     assert.strictEqual(filenode.size(), 2);
+    assert.strictEqual(filenode.height(), 1);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), true);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [7, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree3 fixture and writes to console", () => {
     const filenode = fromYaml("tree3.yml");
 
     assert.strictEqual(filenode.size(), 3);
+    assert.strictEqual(filenode.height(), 1);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), true)
+    assert.strictEqual(filenode.isBalanced(), true);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [7, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 13])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree4 fixture and writes to console", () => {
     const filenode = fromYaml("tree4.yml");
 
     assert.strictEqual(filenode.size(), 4);
+    // assert.strictEqual(filenode.height(), 2)
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), true);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [3, 7, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 3, 13])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree5 fixture and writes to console", () => {
     const filenode = fromYaml("tree5.yml");
 
     assert.strictEqual(filenode.size(), 5);
+    assert.strictEqual(filenode.height(), 2);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), true);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [3, 7, 19, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 3, 13, 19])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree6 fixture and writes to console", () => {
     const filenode = fromYaml("tree6.yml");
 
     assert.strictEqual(filenode.size(), 6);
+    assert.strictEqual(filenode.height(), 3);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), false);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [3, 7, 29, 19, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 3, 13, 19, 29])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree7 fixture and writes to console", () => {
     const filenode = fromYaml("tree7.yml");
 
     assert.strictEqual(filenode.size(), 7);
+    assert.strictEqual(filenode.height(), 3);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), false);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [5, 3, 7, 29, 19, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 3, 5, 13, 19, 29])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree8 fixture and writes to console", () => {
     const filenode = fromYaml("tree8.yml");
 
     assert.strictEqual(filenode.size(), 8);
+    assert.strictEqual(filenode.height(), 3);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), false);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [2, 5, 3, 7, 29, 19, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 3, 2, 5, 13, 19, 29])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree9 fixture and writes to console", () => {
     const filenode = fromYaml("tree9.yml");
 
     assert.strictEqual(filenode.size(), 9);
+    assert.strictEqual(filenode.height(), 3);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), false);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [2, 5, 3, 7, 17, 29, 19, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 3, 2, 5, 13, 19, 17, 29])
+    // expect(tree1.isPathogical).to be false
   });
 
   it("reads tree10 fixture and writes to console", () => {
     const filenode = fromYaml("tree10.yml");
 
     assert.strictEqual(filenode.size(), 10);
+    assert.strictEqual(filenode.height(), 4);
+    assert.strictEqual(filenode.isBst(), true);
+    // assert.strictEqual(filenode.isFull(), false)
+    assert.strictEqual(filenode.isBalanced(), false);
+    // assert.deepStrictEqual(filenode.postOrderKeys(), [2, 5, 3, 7, 17, 23, 29, 19, 13, 11])
+    // assert.deepStrictEqual(filenode.preOrderKeys(), [11, 7, 3, 2, 5, 13, 19, 17, 29, 23])
+    // expect(tree1.isPathogical).to be false
   });
 });
