1e2de232f384bcf7aee0261f2ad5970102eda1c6
dave doolin <david.doolin@gmail.com>
2022-09-17 08:53:02 -0700

    BST-295 initial implementation of racket testing
    This is just an initial, very crude proof-of-concept
    demonstrating one way racket unit testing can be set up
    and run in CI.

---
+++ racket/scribblings/bst.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[bst
+                    racket/base]]
+
+@title{bst}
+@author{doolin}
+
+@defmodule[bst]
+
+Package Description Here
--- racket/testem.rkt
+++ racket/testem.rkt
@@ -3,5 +3,14 @@
 
 ;;; https://docs.racket-lang.org/guide/scripts.html
 
-(printf "Given arguments: ~s\n"
-  (current-command-line-arguments))
+; (printf "Given arguments: ~s\n"
+;   (current-command-line-arguments))
+
+(define (my-+ a b)
+  (if (zero? a) b (my-+ (sub1 a) (add1 b))))
+
+(define (my-* a b)
+  (if (zero? a) b (my-* (sub1 a) (my-+ b b))))
+
+(provide my-+
+         my-*)
