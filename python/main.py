d5c8bb4b6ba1880e4d1c47f7e7f857bb40136283
dave doolin <david.doolin@gmail.com>
2024-04-22 17:11:42 -0700

    BST-329 Python maintenance
    https://doolin.atlassian.net/browse/BST-329
    
    Some maintenance tasks:
    
    - Ensure coverage runs
    - Ensure pytest runs

--- python/main.py
+++ python/main.py
@@ -4,28 +4,39 @@
 
 import argparse
 import sys
-sys.path.append('./lib/')
+
+sys.path.append("./lib/")
 from pyskel import *
+
 
 # http://docs.python.org/library/argparse.html
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Skeleton code for small Python application.')
+    parser = argparse.ArgumentParser(
+        description="Skeleton code for small Python application."
+    )
     # mandatory argument
-    #parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+    # parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
 
-    #optional arguments
-    parser.add_argument("-v", "--verbosity", help="increase the verbosity level of the output", action="store_true")
+    # optional arguments
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        help="increase the verbosity level of the output",
+        action="store_true",
+    )
     args = parser.parse_args()
-    #print args.accumulate(args.integers)
+    # print args.accumulate(args.integers)
 
     # Process the command line arguments
     if args.verbosity:
-        print "verbosity enabled..."
+        print("verbosity enabled...")
+
 
 def run_pyskel():
     ps = PySkel()
-    print ps.foo()
+    print(ps.foo())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parse_arguments()
     run_pyskel()
